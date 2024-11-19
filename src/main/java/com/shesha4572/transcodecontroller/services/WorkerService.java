86fc993a61a3cf27b66d2a5202bebe405a78979f
shesha4572 <shesha4572@gmail.com>
2024-11-19 10:39:57 +0530

    Worker discovery set nodes dead if they dont ping in 30secs

--- src/main/java/com/shesha4572/transcodecontroller/services/WorkerService.java
+++ src/main/java/com/shesha4572/transcodecontroller/services/WorkerService.java
@@ -6,13 +6,17 @@ import com.shesha4572.transcodecontroller.repositories.WorkerPodRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Component
 public class WorkerService {
     public final WorkerPodRepository workerPodRepository;
 
@@ -36,7 +40,7 @@ public class WorkerService {
             }
         }
         else {
-            log.info("Slave {} discovered. ", podName);
+            log.info("Worker {} discovered. ", podName);
             workerPod = WorkerPod.builder()
                     .workerPodName(podName)
                     .isAssignedTask(Boolean.FALSE)
@@ -46,6 +50,18 @@ public class WorkerService {
                     .build();
         }
         workerPodRepository.save(workerPod);
+    }
+
+    @Scheduled(fixedDelay = 35000)
+    public void reconcileDeadWorkers(){
+        log.info("Reconciling dead worker nodes..");
+        workerPodRepository.findAll().forEach(workerPod -> {
+            if(workerPod.getIsAssumedAlive() && workerPod.getLastPinged().plusSeconds(30).isBefore(LocalDateTime.now())){
+                workerPod.setIsAssumedAlive(Boolean.FALSE);
+                log.info("Worker {} has not pinged in {} seconds, Assuming worker is dead" , workerPod.getWorkerPodName() , Duration.between(workerPod.getLastPinged() , LocalDateTime.now()).toSeconds());
+            }
+            workerPodRepository.save(workerPod);
+        });
     }
 
 }
